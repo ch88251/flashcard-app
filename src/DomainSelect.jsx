e9313a7746e8c220ce9c19606e5ea21267e0ddd1
Charles Hayes <cfhayes16@gmail.com>
2026-01-04 19:22:34 -0700

    changed data-testid

--- src/DomainSelect.jsx
+++ src/DomainSelect.jsx
@@ -2,7 +2,7 @@ function DomainSelect({ domains, onSelect }) {
   return (
     <div style={{ textAlign: "center", marginTop: "1rem", marginBottom: "2rem" }}>
       <label htmlFor="domain" className="block mb-2 text-lg font-medium text-indigo-700">Select a Subject: </label>
-      <select data-testid="subject" id="domain" onChange={(e) => onSelect(e.target.value)}>
+      <select data-testid="subject-select" id="domain" onChange={(e) => onSelect(e.target.value)}>
         <option value="">-- Choose One --</option>
         {domains.map((domain, idx) => (
           <option key={idx} value={domain}>
